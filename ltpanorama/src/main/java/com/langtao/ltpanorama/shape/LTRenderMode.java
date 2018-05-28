1f0c705e538eea78334070903e2f7d36b8bb0784
MrZhaozhirong <2377871036@qq.com>
2018-05-28 16:56:59 +0800

    update

--- ltpanorama/src/main/java/com/langtao/ltpanorama/shape/LTRenderMode.java
+++ ltpanorama/src/main/java/com/langtao/ltpanorama/shape/LTRenderMode.java
@@ -22,9 +22,4 @@ public class LTRenderMode {
     public static final int RENDER_MODE_PLANET = 720+3; //小行星
     public static final int RENDER_MODE_VR = 720+5;     //vr 要占用4 5两个值
 
-    public static final int RENDER_MODE_CRYSTAL_NEW = 730;  //水晶球 fbo生成rgb全图
-    public static final int RENDER_MODE_NORMAL_NEW = 730+1;
-    public static final int RENDER_MODE_FISHEYE_NEW = 730+2;
-    public static final int RENDER_MODE_PLANET_NEW = 730+3;
-    public static final int RENDER_MODE_VR_NEW = 730+5;
 }
